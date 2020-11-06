6bf026ae1a9a129c33425f6505cbee042077387a
Ошкина Елена Владимировна <eoshkina@dev.vtb>
2020-11-06 10:20:11 +0300

    fix

--- src/DemoHelloWorld.java
+++ src/DemoHelloWorld.java
@@ -3,6 +3,6 @@ import static javax.swing.JOptionPane.showMessageDialog;
 public class DemoHelloWorld {
   public static void main(String[] args) {
     showMessageDialog(null, "Программируем на Java");
-    //System.out.println("Программируем на Java");
+    //System.out.println("Программируем на Java!");
   }
 }
